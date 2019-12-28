ff8f0a22db3bdb974fb11016e48e1efc98ba5fef
Azat Ateş <38978081+azatates7@users.noreply.github.com>
2019-12-28 12:42:01 +0300

    Update camera.ino

--- camera/camera.ino
+++ camera/camera.ino
@@ -1,1 +1,28 @@
+ #include </home/azatates7//Downloads/Project/camera/module.h>
 
+void setup(){
+  int pirPin = 9;  // PIR pin
+  int deger = 0; // PIR value
+  pinMode(8,OUTPUT);
+  pinMode(pirPin, INPUT);     // PIR Pin'i giriş yapılıyor.
+  Serial.begin(9600);           //Serial Porttan veri göndermek için baundrate ayarlanıyor.
+  arduinoUnoInut();
+  camInit();
+  setResolution();
+  setColor();
+ writeReg(0x11, 10); 
+}
+
+void loop(){ 
+  deger = digitalRead(pirPin);  // Dijital pin okunuyor
+  Serial.println(deger);          // Okunan değer seri porttan okunuyor.
+  if (deger == HIGH) {            
+    captureImg(320, 240);
+    digitalWrite(8,HIGH); 
+    delay(2000);
+    digitalWrite(8, LOW);
+  }
+  else{
+    delay(2000);
+    digitalWrite(8,LOW);
+  } }
